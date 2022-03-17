d00fa5b4932000c697fad6e0a58cc35cce097230
Jake Petersen <jakepetersen1221@gmail.com>
2022-03-17 16:56:53 -0400

    Integration with Tailwind

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
+import './styles/app.css';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,26 @@
+import { useState, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
 export default function Login() {
-  return <p>I am the login page</p>;
+  const history = useNavigate();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - InstaCat';
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <p>I am the login screen</p>
+    </div>
+  );
 }
