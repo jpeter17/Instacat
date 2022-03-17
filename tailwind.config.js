de61948a85bf69323cc7b3b1b71365ab143d4ab5
Jake Petersen <jakepetersen1221@gmail.com>
2022-03-17 17:49:49 -0400

    Completed skeleton for login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#005c98',
+        faded: '#000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    },
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
